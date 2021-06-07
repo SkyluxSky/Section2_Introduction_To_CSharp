3ecdbe9247e17f643b9f9330102144da838a99da
SkyluxSky <skyluxsky@gmail.com>
2021-06-06 20:13:15 -0400

    Hello World C# Program With Beep!

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿//Using statements are equivalent to import statements 
+using System;
+
+
+//namespaces are spaces where we create classes to build our program
+namespace HelloWorld
+{
+    //All C# Programs have a main class called Program
+    class Program
+    {
+        //All Program Classes have a Main Method 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.Beep(400, 8000);
+            Console.WriteLine("This is a Beep");
+        }
+    }
+}
